3a498c9a83e79feaa4a0b639939066a54938b5ec
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-03-20 21:52:41 +0000

    Updated Render Buffer class to use multiple color buffers.

--- 3DGP/RenderTexture.cpp
+++ 3DGP/RenderTexture.cpp
@@ -2,24 +2,34 @@
 
 #include <stdexcept>
 
-RenderTexture::RenderTexture(int _width, int _height)
+RenderTexture::RenderTexture(const int _width, const int _height, const unsigned int _colorBufferCount)
 {
 	// Generate and bind framebuffer object.
 	glGenFramebuffers(1, &m_fboId);
-	if (!m_fboId) { throw std::runtime_error("Failed to load Frame Buffer Object in Render Texture."); }
+	if (!m_fboId) { throw std::runtime_error("Failed to load frame buffer object in render texture."); }
 	glBindFramebuffer(GL_FRAMEBUFFER, m_fboId);
 
-	// Generate and bind texture.
-	glGenTextures(1, &m_textureId);
-	glBindTexture(GL_TEXTURE_2D, m_textureId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-
-	// The render texture should not generate minmaps.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	if (_colorBufferCount < 1) { throw std::runtime_error("Tried to generate an amount of color buffers exceeding the minimum."); }
+	if (_colorBufferCount > c_maxBuffers) { throw std::runtime_error("Tried to generate an amount of color buffers exceeding the maximum."); }
 	
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_textureId, 0);
+	// Generate and bind texture.
+	glGenTextures(_colorBufferCount, m_textureIds);
+	for(unsigned int i = 0; i < _colorBufferCount; i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, m_textureIds[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+		// The render texture should not generate minmaps.
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, m_textureIds[i], 0);
+
+		m_attachments[i] = GL_COLOR_ATTACHMENT0 + i;
+	}
 
 	// Generate and bind render buffer.
 	glGenRenderbuffers(1, &m_rboId);
@@ -33,11 +43,25 @@ RenderTexture::RenderTexture(int _width, int _height)
 	
 	// Unbind framebuffer object.
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	m_bufferCount = _colorBufferCount;
 }
 
 RenderTexture::~RenderTexture()
 {
 	glDeleteRenderbuffers(1, &m_rboId);
-	glDeleteTextures(1, &m_textureId);
+	glDeleteTextures(m_bufferCount, m_textureIds);
 	glDeleteFramebuffers(1, &m_fboId);
+}
+
+void RenderTexture::Bind() const
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, m_fboId);
+	glDrawBuffers(m_bufferCount, m_attachments);
+}
+
+void RenderTexture::Unbind()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glDrawBuffer(GL_COLOR_ATTACHMENT0);
 }
--- 3DGP/RenderTexture.h
+++ 3DGP/RenderTexture.h
@@ -5,16 +5,25 @@
 class RenderTexture
 {
 private:
+	static constexpr unsigned int c_maxBuffers = 4;
+	
 	GLuint m_fboId;
-	GLuint m_textureId;
+	GLuint m_textureIds[c_maxBuffers];
 	GLuint m_rboId;
+
+	unsigned int m_attachments[c_maxBuffers];
+
+	unsigned int m_bufferCount;
 public:
-	RenderTexture(int _width, int _height);
+	RenderTexture(int _width, int _height, unsigned int _colorBufferCount = 1);
 	~RenderTexture();
 
 	RenderTexture(const RenderTexture& _copy) = delete;
 	RenderTexture& operator=(const RenderTexture& _other) = delete;
 
-	[[nodiscard]] GLuint GetTexture() const { return m_textureId; }
+	void Bind() const;
+	static void Unbind();
+
+	[[nodiscard]] GLuint GetTexture(unsigned int _index) const { return m_textureIds[_index]; }
 };
 
