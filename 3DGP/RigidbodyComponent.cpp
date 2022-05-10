2771897d7de86a04b0bb8a2d6e8d93fa794d9def
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-05-10 22:35:41 +0100

    Added rigidbody member variables

--- 3DGP/RigidbodyComponent.cpp
+++ 3DGP/RigidbodyComponent.cpp
@@ -2,11 +2,20 @@
 
 #include "Resources.h"
 
+RigidbodyComponent::RigidbodyComponent()
+{
+	m_force = glm::vec3(0.0f);
+	m_velocity = glm::vec3(0.0f);
+	m_torque = glm::vec3(0.0f);
+
+	m_mass = 0.0f;
+}
+
 std::shared_ptr<IComponent> RigidbodyComponent::Parser::Parse(rapidjson::Value& _value)
 {
 	std::shared_ptr<RigidbodyComponent> component = std::make_unique<RigidbodyComponent>();
 
-	// Set component values
+	component->SetMass(_value["mass"].GetFloat());
 
 	return std::static_pointer_cast<IComponent>(component);
 }
--- 3DGP/RigidbodyComponent.h
+++ 3DGP/RigidbodyComponent.h
@@ -7,6 +7,12 @@ class Resources;
 
 class RigidbodyComponent : public IComponent
 {
+private:
+	glm::vec3 m_force;
+	glm::vec3 m_velocity;
+	glm::vec3 m_torque;
+
+	float m_mass;
 public:
 	class Parser final : public IJsonParser<IComponent>
 	{
@@ -14,5 +20,25 @@ public:
 		explicit Parser(Resources* _resources) : IJsonParser(_resources) {}
 		std::shared_ptr<IComponent> Parse(rapidjson::Value& _value) override;
 	};
+
+	RigidbodyComponent();
+
+	void AddForce(const glm::vec3 _force) { m_force += _force; }
+	void AddTorque(const glm::vec3 _torque) { m_torque += _torque; }
+
+	void ClearForce() { SetForce(glm::vec3(0.0f)); }
+	void ClearTorque() { SetTorque(glm::vec3(0.0f)); }
+
+	glm::vec3 GetForce() const { return m_force; }
+	glm::vec3 GetVelocity() const { return m_velocity; }
+	glm::vec3 GetTorque() const { return m_torque; }
+	
+	float GetMass() const { return m_mass; }
+
+	void SetForce(const glm::vec3 _force) { m_force = _force; }
+	void SetVelocity(const glm::vec3 _velocity) { m_velocity = _velocity; }
+	void SetTorque(const glm::vec3 _torque) { m_torque = _torque; }
+	
+	void SetMass(const float _mass) { m_mass = _mass; }
 };
 
